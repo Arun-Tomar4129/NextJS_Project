3cb242d9120fadc421ffb3f41746a27a9d8aab68
Arun-Tomar4129 <arunrajput412995@gmail.com>
2025-07-24 16:51:35 +0530

    fix issuse

--- app/(client)/Webpage/Navbar.tsx
+++ app/(client)/Webpage/Navbar.tsx
@@ -4,17 +4,16 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Menu, X } from "lucide-react";
 import Link from "next/link";
-import { label } from "framer-motion/client";
+import Image from "next/image";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const navLinks = [
-     {href: "/Webpage", label: "Home"},
+    { href: "/Webpage", label: "Home" },
     { href: "/Webpage/Course", label: "Courses" },
     { href: "/Webpage/Placement", label: "Placement" },
     { href: "/Webpage/About", label: "About" },
-   
   ];
 
   return (
@@ -25,14 +24,16 @@ const Navbar = () => {
       className="w-full bg-white shadow-md fixed top-0 z-50"
     >
       <div className="w-full flex justify-between items-center px-[5px] lg:px-[10px] py-3">
-
         {/* Logo & Name */}
         <div className="flex items-center gap-3">
-          <img
+          <Image
             src="/logoUnivercity.jpeg"
             alt="University Logo"
-            className="w-[40px] h-[40px] rounded-full object-cover"
+            width={40}
+            height={40}
+            className="rounded-full object-cover"
           />
+
           <h1 className="text-lg sm:text-xl font-semibold text-indigo-700">
             Radhe Shyam University
           </h1>
@@ -65,9 +66,13 @@ const Navbar = () => {
       {/* Mobile Dropdown Menu */}
       <motion.div
         initial={{ height: 0, opacity: 0 }}
-        animate={isOpen ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }}
+        animate={
+          isOpen ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }
+        }
         transition={{ duration: 0.3 }}
-        className={`md:hidden bg-white overflow-hidden px-6 ${isOpen ? "pb-4" : "pb-0"} flex flex-col gap-4 text-gray-700 font-medium`}
+        className={`md:hidden bg-white overflow-hidden px-6 ${
+          isOpen ? "pb-4" : "pb-0"
+        } flex flex-col gap-4 text-gray-700 font-medium`}
       >
         {navLinks.map((link) => (
           <Link
